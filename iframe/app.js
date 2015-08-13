8c388741f1f0a569aecbb68c274d9e3f81786295
Joseph McCarron <joe@zendesk.com>
2015-08-13 14:34:56 -0700

    demo app changes

--- iframe/app.js
+++ iframe/app.js
@@ -1,19 +1,33 @@
 // app.js
 (function() {
   var app = angular.module('theEye', ["firebase", "angularMoment"]);
-      // #### Firebase
-  // var url = "https://theeyeofzen.firebaseio.com/accounts/appsonappsonapps/agents",
-  //     agentsRef = new Firebase( url );
-  
-
   // Controllers
 
   app.controller('AgentsController', ['$scope', '$firebase',
     function($scope, $firebase) {
       $scope.agents = $firebase(agentsRef).$asArray();
       $scope.subdomain = account.subdomain;
-      console.dir($scope.agents);
-
+      // console.dir($scope.agents);
     }
   ]);
+  app.controller('TicketsController', ['$scope', '$firebase',
+    function($scope, $firebase) {
+      $scope.subdomain = account.subdomain;
+
+      var agents = $firebase(agentsRef).$asArray();
+      agents.$watch(function(x) {
+        // console.dir(agents);
+        var ticketArrays = _.map(agents, function(agent) {
+          return _.toArray( agent.tickets );
+        });
+        console.dir(ticketArrays);
+        $scope.tickets = _.flatten(ticketArrays);
+
+        console.dir($scope.tickets);
+      });
+      
+    }
+  ]);
+
+
 })();
